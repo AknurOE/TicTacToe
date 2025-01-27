bb89b950b9c01718d60fe5aa4b860e5cd72f76c0
AknurOE <ondasssynova06@gmail.com>
2025-01-27 22:40:17 +0500

    Add files via upload
    added PostGreSQL database

---
+++ proj/src/Board.java
@@ -0,0 +1,55 @@
+// боард класс для инкапсуляции поля
+class Board {
+    private final char[][] grid; // массив чтоб храненить состояние поля
+    private static final int SIZE = 3;
+
+    public Board() {
+        grid = new char[SIZE][SIZE]; // создаю массив
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                grid[i][j] = '-'; // который изначально ячейки пустые
+            }
+        }
+    }
+
+    public boolean makeMove(int row, int col, char symbol) {
+        if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && grid[row][col] == '-') {
+            grid[row][col] = symbol;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkWin(char symbol) {
+        // проверить роус и колумнс
+        for (int i = 0; i < SIZE; i++) {
+            if ((grid[i][0] == symbol && grid[i][1] == symbol && grid[i][2] == symbol) ||
+                    (grid[0][i] == symbol && grid[1][i] == symbol && grid[2][i] == symbol)) {
+                return true;
+            }
+        }
+        // проверка диагоналей
+        return (grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol) ||
+                (grid[0][2] == symbol && grid[1][1] == symbol && grid[2][0] == symbol);
+    }
+
+    public boolean isFull() {
+        for (char[] row : grid) {
+            for (char cell : row) {
+                if (cell == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void display() {
+        for (char[] row : grid) {
+            for (char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
